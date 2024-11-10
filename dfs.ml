b1ade1bbf595ab23ac1c93467077e2c0ef0e10eb
Tayba <tayba.jusab25@gmail.com>
2024-11-10 13:04:22 -0500

    Fix DFS code

--- dfs.ml
+++ dfs.ml
@@ -1,13 +1,13 @@
+type 'a graph = { 
+  nodes: 'a list;
+  edges: ('a * 'a) list
+}
+
 (* White is unvisited
    Grey is currently in DFS stack
    Black is fully explored
 *)
 type color = White | Grey | Black
-
-type 'a graph = { 
-  nodes: ('a * color ref) list;
-  edges: ('a * 'a) list
-}
 
 (* We want to use DFS to detect cycles in an undirected graph *)
 (*
@@ -20,6 +20,9 @@ type 'a graph = {
     4. Mark the node as Black after exploring all its neighbors
 *)
 
+let initialize_graph (graph: 'a graph): ('a * color ref) list =
+  List.map(fun node -> (node, ref White)) graph.nodes
+
 let find_neighbors (graph: 'a graph) (vertex: 'a): 'a list =
   List.fold_left (fun acc (u, v) ->
       if u = vertex then v :: acc
@@ -27,16 +30,16 @@ let find_neighbors (graph: 'a graph) (vertex: 'a): 'a list =
       else acc
     ) [] graph.edges
 
-let rec dfs (graph: 'a graph) (vertex: 'a) (parent: 'a option): bool =
-  let vertex_color = List.assoc vertex graph.nodes in
+let rec dfs (graph: 'a graph) (color_map: ('a * color ref) list) (vertex: 'a) (parent: 'a option): bool =
+  let vertex_color = List.assoc vertex color_map in
   vertex_color := Grey;
   
   let neighbors = find_neighbors graph vertex in
   List.exists (fun neighbor ->
-      let neighbor_color = List.assoc neighbor graph.nodes in
+      let neighbor_color = List.assoc neighbor color_map in
       match !neighbor_color with
       | White ->
-          if dfs graph neighbor (Some vertex) then true else false 
+          if dfs graph color_map neighbor (Some vertex) then true else false 
       | Grey ->
           (* If the neighbor is Grey and not the parent, cycle found *)
           (match parent with
@@ -46,7 +49,8 @@ let rec dfs (graph: 'a graph) (vertex: 'a) (parent: 'a option): bool =
     ) neighbors
   
 let detect_cycle (graph: 'a graph): bool = 
+  let color_map = initialize_graph graph in
   List.exists (fun (vertex, color) ->
-      if !color = White then dfs graph vertex None
+      if !color = White then dfs graph color_map vertex None
       else false
-    ) graph.nodes
+    ) color_map
