b7f5432e38f0b1f87a3a7df808b7e70579620912
James Luu <jamesluu0917@gmail.com>
2024-11-11 03:46:41 -0500

    finished template code for dfs

--- dfs-template-code.ml
+++ dfs-template-code.ml
@@ -1,16 +1,16 @@
 
 exception NotImplemented
 
+type 'a graph = { 
+  nodes: ('a * color ref) list;
+  edges: ('a * 'a) list
+}
+
 (* White is unvisited
    Grey is currently in DFS stack
    Black is fully explored
 *)
 type color = White | Grey | Black
-
-type 'a graph = { 
-  nodes: ('a * color ref) list;
-  edges: ('a * 'a) list
-}
 
 (* We want to use DFS to detect cycles in an undirected graph *)
 (*
@@ -23,21 +23,69 @@ type 'a graph = {
     4. Mark the node as Black after exploring all its neighbors
 *)
 
-let find_neighbors_tests: (('a graph * 'a) * 'a list) list = []
+(**
+  Initializes the color map for each node in the graph.
+  
+  @param graph The input graph.
+  @return A list of pairs containing a node and a reference to its color (initially White).
+          This list will be used to track the status of each node during DFS traversal.
+*)
+let initialize_graph (graph: 'a graph): ('a * color ref) list = List.map(fun node -> (node, ref White)) graph.nodes
 
-(* Use List.fold_left *)
+let find_neighbors_tests: (('a graph * 'a) * 'a list) list = [
+  (({ nodes = ['a'; 'b'; 'c']; edges = [('a', 'b'); ('b', 'c')] }, 'a'), ['b']);
+  (({ nodes = ['a'; 'b'; 'c']; edges = [('a', 'b'); ('b', 'c')] }, 'b'), ['a'; 'c']);
+  (({ nodes = ['a'; 'b'; 'c']; edges = [('a', 'b'); ('b', 'c')] }, 'c'), ['b']);
+  (({ nodes = ['a'; 'b'; 'c'; 'd']; edges = [('a', 'b'); ('b', 'c'); ('c', 'd'); ('d', 'a')] }, 'd'), ['a'; 'c']);
+  (({ nodes = ['a'; 'b'; 'c']; edges = [] }, 'a'), [])
+]
+
+(** 
+  Finds all neighbors of a given vertex in an undirected graph.
+  
+  @param graph The input graph.
+  @param vertex The node for which neighbors are to be found.
+  @return A list of neighbors directly connected to the given vertex.
+*)
 let find_neighbors (graph: 'a graph) (vertex: 'a): 'a list =
   raise NotImplemented
 
-let dfs_tests: (('a graph * 'a * 'a option) * bool) list = []
+let dfs_tests: (('a graph * (('a * color ref) list) * 'a * 'a option) * bool) list = [
+  (({ nodes = ['a'; 'b']; edges = [('a', 'b')] }, [('a', ref White); ('b', ref White)], 'a', None), false);
+  (({ nodes = ['a'; 'b'; 'c']; edges = [('a', 'b'); ('b', 'c'); ('c', 'a')] }, [('a', ref White); ('b', ref White); ('c', ref White)], 'a', None), true);
+  (({ nodes = ['a'; 'b'; 'c']; edges = [('a', 'b')] }, [('a', ref White); ('b', ref White); ('c', ref White)], 'b', None), false);
+  (({ nodes = ['a']; edges = [] }, [('a', ref White)], 'a', None), false);
+]
 
-(* May use List.assoc to look up and edit vertex color *)
-(* Use List.exists *)
-let rec dfs (graph: 'a graph) (vertex: 'a) (parent: 'a option): bool =
+(** 
+  Performs a depth-first search (DFS) from a given vertex to detect cycles in an undirected graph.
+
+  @param graph The input graph.
+  @param color_map A list of pairs containing a node and its mutable color reference to track its status.
+  @param vertex The current node being visited in the DFS traversal.
+  @param parent Previous node in the DFS path.
+  @return A boolean value indicating whether a cycle is detected:
+          - `true` if a cycle is found,
+          - `false` otherwise.
+*)
+let rec dfs (graph: 'a graph) (color_map: ('a * color ref) list) (vertex: 'a) (parent: 'a option): bool =
   raise NotImplemented
 
-let detect_cyle_tests: ('a graph * bool) list = []
+let detect_cyle_tests: ('a graph * bool) list = [
+  ({ nodes = ['a'; 'b']; edges = [('a', 'b')] }, false);
+  ({ nodes = ['a'; 'b'; 'c']; edges = [('a', 'b'); ('b', 'c'); ('c', 'a')] }, true);
+  ({ nodes = ['a'; 'b'; 'c']; edges = [('a', 'b')] }, false);
+  ({ nodes = ['a']; edges = [] }, false);
+]
 
-(* Use List.exists *)
+(** 
+  Detects if there is a cycle in the graph by applying DFS to each unvisited node.
+  Can be implemented by first initializing a color map, then calling DFS on each unvisited node to determine if cycle exists.
+  
+  @param graph The input graph.
+  @return A boolean value indicating whether the graph contains a cycle:
+          - `true` if a cycle is detected,
+          - `false` otherwise.
+*)
 let detect_cycle (graph: 'a graph): bool = 
   raise NotImplemented
